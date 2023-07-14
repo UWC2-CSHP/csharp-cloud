9d06c1ea941436d3c2c87b131031b21d2261d1ac
Daniel Vallejo <danv@cstructor.com>
2023-07-13 20:58:11 -0700

    Add Controller

--- HelloWorldService/HelloWorldService/Controllers/WeatherForecastController.cs
+++ HelloWorldService/HelloWorldService/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace HelloWorldService.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Calling get {0}", 1);
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
