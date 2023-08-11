45fb6d4c733984d19c9d0e42e84be201702bed38
Daniel Vallejo <danv@cstructor.com>
2023-08-10 18:35:50 -0700

    Add unit test exercises

--- HelloWorldService.Tests/UnitTest1.cs
+++ HelloWorldService.Tests/UnitTest1.cs
@@ -1,11 +1,7 @@
 using System.Text.Json;
 using FluentAssertions;
-using NUnit.Framework;
-using System;
 using System.Net;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorldService.Tests
 {
@@ -49,5 +45,112 @@ namespace HelloWorldService.Tests
 
             postResult.StatusCode.Should().Be(HttpStatusCode.Created);
         }
+
+        [Test]
+        public async Task TestGetAll()
+        {
+            var result = await client.GetAsync("contacts");
+
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task TestDelete_ValidContactId()
+        {
+            var postResult = await CreateContact("TestDelete_ValidContactId");
+
+            var json = await postResult.Content.ReadAsStringAsync();
+
+            var contact = JsonSerializer.Deserialize<Contact>(json);
+
+            var result = await client.DeleteAsync("contacts/" + contact.Id);
+
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task TestDelete_InvalidContactStringId()
+        {
+            var result = await client.DeleteAsync("contacts/abc");
+
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Test]
+        public async Task TestDelete_InvalidContactId()
+        {
+            var result = await client.DeleteAsync("contacts/1");
+
+            result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Test]
+        public async Task TestGetSpecific_Good()
+        {
+            var postResult = await CreateContact("TestGetSpecific_Good");
+
+            var json = await postResult.Content.ReadAsStringAsync();
+
+            var contact = JsonSerializer.Deserialize<Contact>(json);
+
+            var result = await client.GetAsync("contacts/" + contact.Id);
+
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task TestGetSpecific_Bad()
+        {
+            var result = await client.GetAsync("contacts/10211");
+
+            result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Test]
+        public async Task TestAddNewContact()
+        {
+            var postResult = await CreateContact("AddNewContactTest");
+
+            Assert.AreEqual(HttpStatusCode.Created, postResult.StatusCode);
+
+            postResult.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+
+        [Test]
+        public async Task TestAddNewContact_NameNull()
+        {
+            var postResult = await CreateContact(null);
+
+            postResult.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Test]
+        public async Task TestAddNewContact_NameEmpty()
+        {
+            var postResult = await CreateContact("");
+
+            postResult.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        private async Task<HttpResponseMessage> CreateContact(string name)
+        {
+            var newContact = new Contact
+            {
+                Name = name,
+                Phones = new[] {
+                    new Phone {
+                        Number = "425-111-2222",
+                        PhoneType = PhoneType.Mobile
+                    }
+                }
+            };
+            var newJson = JsonSerializer.Serialize(newContact);
+            var postContent = new StringContent(newJson, Encoding.UTF8, "application/json");
+            var postResult = await client.PostAsync("contacts", postContent);
+
+            return postResult;
+        }
+
+
     }
 }
