955e131437a239a8ede33cb781957bb5415bb89e
Daniel Vallejo <danv@cstructor.com>
2023-09-08 11:28:14 -0700

    add TokenResponse

--- MinimalTokenService/Token.cs
+++ MinimalTokenService/Token.cs
@@ -1,9 +1,17 @@
-﻿namespace MinimalTokenService
+﻿using System.Text.Json.Serialization;
+
+namespace MinimalTokenService
 {
     public class TokenRequest
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+    }
+
+    public class TokenResponse
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
     }
 
     public class Token
